b6157c991b2c4c315890f3bc5c64a3ea8216177c
Amulya Kumar Shahi <amulyaa_24@yahoo.in>
2020-11-23 18:40:30 +0530

    regex formatter done

--- day3/repeatation.rb
+++ day3/repeatation.rb
@@ -3,19 +3,14 @@
 def dupli_char(strin)
 	str1 = []
 	dist = []
-
 	str1 = strin.chars
-
 	str1.each do |v|
 		if str1.first == v
 			dist<<v
 		end
 	end
-
 	puts "#{dist.first}, found #{dist.count} times." 
 
 end
-
-
 
 dupli_char("aaabse")
